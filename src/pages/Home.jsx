474033fe784f5dbbecd288b34431d47d979a1293
Gisella Rosado <gisella_rosado@hotmail.com>
2023-05-05 23:14:45 -0500

    development save -advances with the welcome page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Welcome from '../components/Welcome';
+
+import '../styles/Home/home.css'
+
+const Home = () => {
+	return (
+		<div>
+			<Welcome />
+		</div>
+	);
+}
+
+export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home.jsx';
+import Play from '../pages/Play';
+import HighScores from '../pages/HighScores';
+import NotFound from '../pages/NotFound';
+import Layout from '../container/Layout.jsx';
+
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Layout>
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+            <Route exact path="/play" element={<Play />} />
+            <Route exact path="/highscores" element={<HighScores />} />
+            <Route exact path="/notfound" element={<NotFound />} />
+					</Routes>
+        </Layout>
+		  </BrowserRouter>
     </div>
   );
 }
